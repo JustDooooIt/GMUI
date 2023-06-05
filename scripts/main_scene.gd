59142062572ed57521aaed7f4d76cd9abd06bc8d
JustDooooIt <844730658@qq.com>
2023-06-05 18:44:59 +0800

    完善

--- scripts/main_scene.gd
+++ scripts/main_scene.gd
@@ -1,10 +1,10 @@
 extends "res://addons/go_vm/scripts/common/root_node_2d.gd"
 
-
 @onready var data = vm.define_reactive({'visible': false})
 	
 func _mounted():
-#	data.rset('visible', true)
+	await get_tree().create_timer(5).timeout
+	data.rset('visible', true)
 	print('mounted')
 
 func _updated():
