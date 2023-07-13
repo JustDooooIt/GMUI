a09c27b071379ec12530a6db7f5905637ada8064
JustDooooIt <844730658@qq.com>
2023-07-13 21:30:44 +0800

    fix bug

--- addons/gmui/dist/scripts/pages/index.gd
+++ addons/gmui/dist/scripts/pages/index.gd
@@ -1,7 +1,8 @@
 extends "res://addons/gmui/dist/super_scripts/pages/index.gd"
 
 
-
-
 func _mounted():
-	var ref = gmui.refs['component']
+	gmui.refs['my_button'].rnode.pressed.connect(
+		func():
+			print(gmui.refs['my_button'].rnode.text)
+	)
--- addons/gmui/dist/scripts/pages/index1.gd
+++
@@ -1,8 +0,0 @@
-extends "res://addons/gmui/dist/super_scripts/pages/index1.gd"
-
-
-func _mounted():
-	gmui.refs['my_button'].rnode.pressed.connect(
-		func():
-			print(gmui.refs['my_button'].rnode.text)
-	)
