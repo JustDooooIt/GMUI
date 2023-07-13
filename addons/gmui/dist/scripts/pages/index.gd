1c54d2ff8f354d0d90e621c678631830b31e665c
JustDooooIt <844730658@qq.com>
2023-07-13 16:29:39 +0800

    fix bugs

--- addons/gmui/dist/scripts/pages/index.gd
+++ addons/gmui/dist/scripts/pages/index.gd
@@ -1,16 +1,4 @@
 extends "res://addons/gmui/dist/super_scripts/pages/index.gd"
 
 
-var data = await reactive({'firstName': '张', 'lastName': '四'})
 
-func fullName():
-	return data.rget('firstName') + data.rget('lastName')
-
-func _ready():
-	computed(fullName)
-
-func _mounted():
-	gmui.refs['btn'].rnode.pressed.connect(
-		func():
-			data.rset('firstName', '李')
-	)
